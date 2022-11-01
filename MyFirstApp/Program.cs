919478d8911f4675b7b77deba7d42af182cd5bf2
avvakumov <avvakumov@tslog.ru>
2022-11-01 19:41:12 +0300

    Добавлены символы # и @

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,7 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string myName;
-myName = "Mike";
+const string MyName = "Jane";
 
-Console.WriteLine(myName);
+Console.WriteLine(MyName);
 
+Console.WriteLine("\t Привет Мир");
+Console.WriteLine("\t Мне 27 лет");
+Console.WriteLine("\t My name is \n {0}", MyName);
+Console.WriteLine("\u0040");
+Console.WriteLine("\u0023");
 Console.ReadKey();
