6a38ab0d45a8a1190a7665129e559fafd1d685f9
avvakumov <avvakumov@tslog.ru>
2022-11-07 19:17:44 +0300

    Использован безымянный кортеж

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,10 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-(string name, int age) anketa;
+//(string name, int age) anketa;
+var (name, age) = ("Евгения", 27);
+Console.WriteLine($"Имя и возраст: {name} и {age}");
 Console.Write("Enter your name: ");
-anketa.name = Console.ReadLine();
+name = Console.ReadLine();
 Console.Write("Enter your age: ");
-anketa.age = checked((byte)int.Parse(Console.ReadLine()));
+age = checked((byte)int.Parse(Console.ReadLine()));
 Console.Write("When is your birhtday? ");
 var birthday = Console.ReadLine();
 Console.WriteLine($"Your name is {name}, age is {age} and your birthday is {birthday}");
