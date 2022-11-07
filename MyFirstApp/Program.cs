a4df46e56ef12afcbdb7869064eba373e6a8af53
avvakumov <avvakumov@tslog.ru>
2022-11-07 19:12:42 +0300

    Анкета записывается в кортеж

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,9 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+(string name, int age) anketa;
 Console.Write("Enter your name: ");
-var name = Console.ReadLine();
+anketa.name = Console.ReadLine();
 Console.Write("Enter your age: ");
-var age = checked((byte)int.Parse(Console.ReadLine()));
+anketa.age = checked((byte)int.Parse(Console.ReadLine()));
 Console.Write("When is your birhtday? ");
 var birthday = Console.ReadLine();
 Console.WriteLine($"Your name is {name}, age is {age} and your birthday is {birthday}");
