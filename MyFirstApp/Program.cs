0df7d7ac057c6cdf253f9d56f9cb23080d4eaca5
avvakumov <avvakumov@tslog.ru>
2022-11-17 14:21:06 +0300

    Программа Эхо

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -225,7 +225,6 @@
 //}
 
 
-int buffer = 0;
 
 Console.WriteLine("Напишите что-то");
 var str = Console.ReadLine();
@@ -246,32 +245,11 @@ static void Echo(string saidworld, int deep)
         modif = modif.Remove(0, 2);
     }
     Console.WriteLine("..." + modif);
-for (int i = 0; i < arr.GetUpperBound(0) + 1; i++)
-{
-    for (int j = 0; j < arr.GetUpperBound(1) + 1; j++)
-    {
-        for (int f = j + 1; f < arr.GetLength(1); f++)
-        {
-            if (arr[i, j] > arr[i, f])
-            {
-                buffer = arr[i, j];
-                arr[i, j] = arr[i, f];
-                arr[i, f] = buffer;
-            }
-        }
-    }
-}
+
 
     if (deep > 1)
     {
         Echo(modif, deep - 1);
     }
 }
-for (int i = 0; i < arr.GetUpperBound(0) + 1; i++)
-{
-    for (int j = 0; j < arr.GetUpperBound(1) + 1; j++)
-    {
-        Console.Write(arr[i, j] + "\t");
-    }
-    Console.WriteLine();
-}
+
